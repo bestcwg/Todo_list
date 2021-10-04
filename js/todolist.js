24e2533e54a5fa4091c6b21bff2c0e5860f7231d
Christian Wolter Grann <202009898@post.au.dk>
2021-10-05 00:57:09 +0200

    [ADDED] localstorage

--- js/todolist.js
+++ js/todolist.js
@@ -1,10 +1,44 @@
+window.onload = () => {
+    loadTaskFromStorage();
+    console.log(myStorage.length);
+    console.log(myStorage);
+}
+
 let myStorage = window.localStorage;
 
 function addTaskToList() {
-    let task = document.getElementById("item");
-    let itemList = document.getElementsByClassName("thelist");
-
-    console.log(task);
-    console.log(itemList.length);
+    let task = document.forms.test.item.value;
     
+    //let itemList = document.forms.list.thelist.value;
+    let itemList = document.getElementById("thelist");
+
+    itemList.innerHTML += `<li> ${task} </li>`;
+
+
+    addTaskToStorage(task);
+    console.log(task);
+    console.log(itemList);
+    console.log(myStorage);
 }
+
+function addTaskToStorage(item) {
+    let key = Math.random();
+    myStorage.setItem(key, item);
+}
+
+function loadTaskFromStorage() {
+    if (myStorage.length > 0) {
+        for (let key in myStorage) {
+            // Checks for only returning the actual keys and not setItems etc.
+            if (!myStorage.hasOwnProperty(key)) {
+                continue;
+            }
+            let itemList = document.getElementById("thelist");
+            itemList.innerHTML += `<li> ${myStorage.getItem(key)} </li>`;
+        }
+    }
+}
+
+
+
+
