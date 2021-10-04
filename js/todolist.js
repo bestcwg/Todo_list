9982618a3ebdad946b13681c9468bced701c33dd
Christian Grann <CWG@live.dk>
2021-10-04 18:21:39 +0200

    test

--- js/todolist.js
+++ js/todolist.js
@@ -1,1 +1,10 @@
 let myStorage = window.localStorage;
+
+function addTaskToList() {
+    let task = document.getElementById("item");
+    let itemList = document.getElementsByClassName("thelist");
+
+    console.log(task);
+    console.log(itemList.length);
+    
+}
