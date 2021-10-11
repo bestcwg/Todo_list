601d759e052883efb8ba3afe7967729f26c465d8
Christian Wolter Grann <202009898@post.au.dk>
2021-10-11 21:29:39 +0200

    Finished functionality

--- js/todolist.js
+++ js/todolist.js
@@ -1,125 +1,82 @@
-window.onload = function() {
-    loadTaskFromStorage(myStorage);
-    console.log(myStorage.getItem("list"));
-}
-
-window.onunload = function() {
-    addTasksToStorage(myStorage);
-}
-
 let myStorage = window.localStorage;
+let itemList = JSON.parse(myStorage.getItem("list")) || [];
+let newItemList = [];
+const template = document.querySelector("#task");
+const createTaskSubmit = document.querySelector("#submit");
+const taskField = document.querySelector("#inputfield");
 
-function addTaskToList() {
-    const task = document.forms.textinput.inputfield.value;
+// Add functionality to "add task button"
+createTaskSubmit.addEventListener('click', function() {
+    const value = taskField.value;
+    if (value) {
+        const task = {
+            item: value,
+            checked: false,
+            id: Math.random()
+        }
+        addTask(task);
+    }
+})
+
+// Creates to do list from local storage
+for(let i = 0; i < itemList.length; i++) {
+    addTask(itemList[i]);
+}
+
+function addTask(task) {
+    const clone = template.content.cloneNode(true);
+    const taskListChecks = clone.querySelector("li");
+    const taskText = clone.querySelector("li p");
+    const taskCheck = clone.querySelector("li #checkbox");
+    const taskDeleteButton = clone.querySelector(".close");
     let taskList = document.getElementById("thelist");
-    if (task !== "")  {
-        let newItem = document.createElement("li");
-        newItem.innerHTML = task;
-        createDeleteButton(newItem);
-        createEditButton(newItem);
 
-        taskList.appendChild(newItem);
-        //addTaskToStorage(task);
-        //itemList.push(task);
+    if (task.item !== "")  {
+        taskText.innerHTML = task.item;
+        taskCheck.checked = task.checked;
+
+        if (taskCheck.checked === true) {
+            taskListChecks.classList.toggle("checked");
+        }
+
+        newItemList.push(task);
+        let index = getIndexOf(task);
+
+        // Can edit tasks and saves it to lists which is stored in localstorage
+        taskText.addEventListener("input", function() {
+            task.item = taskText.innerHTML;
+
+            newItemList[index] = task;
+            myStorage.setItem("list", JSON.stringify(newItemList));
+        });
+
+        // Can delete tasks
+        taskDeleteButton.addEventListener("click", function() {
+            let item = this.parentElement;
+            
+            item.remove();
+            newItemList.splice(index, 1);
+            myStorage.setItem("list", JSON.stringify(newItemList));
+        });
+
+        // Can check completed items
+        taskCheck.addEventListener("click", function() {
+            newItemList[index].checked = taskCheck.checked;
+            let item = this.parentElement;
+
+            item.classList.toggle("checked");
+            myStorage.setItem("list", JSON.stringify(newItemList));
+        })
+
+        taskList.appendChild(clone);
+        myStorage.setItem("list", JSON.stringify(newItemList));
     }
 }
 
-function addTasksToStorage(storage) {
-    storage.removeItem("list");
-    let itemList = new Array;
-
-    document.querySelectorAll("#thelist li").forEach(item =>
-        itemList.push(item.textContent));
-    
-    storage.setItem("list", itemList);
-}
-
-function loadTaskFromStorage(storage) {
-    /*if (storage.length > 0) {
-        for (let key in storage) {
-            // Checks for only returning the relevant keys and not setItems etc.
-            if (!storage.hasOwnProperty(key)) {
-                continue;
-            }
-            let storageTaskList = document.getElementById("thelist");
-            let newItem = document.createElement("li");
-           
-            newItem.innerHTML = storage.getItem(key);
-            createDeleteButton(newItem);
-            createEditButton(newItem);
-            storageTaskList.appendChild(newItem);
-        }*/
-    
-    if (storage.getItem("list")) {
-        let items = storage.getItem("list");
-        let storageTaskList = document.getElementById("thelist");
-     
-        console.log(items);
-        for (let i = 0; i < items.length; i++) {
-            let newItem = document.createElement("li");
-            newItem.innerHTML = items[i];
-            createDeleteButton(newItem);
-            createEditButton(newItem);
-            storageTaskList.appendChild(newItem);
+function getIndexOf(task) {
+    for (let i = 0; i < newItemList.length;i++) {
+        if (newItemList[i].id === task.id) {
+            return i;
         }
     }
-    
-    let test = document.querySelector("#thelist");
-    test.addEventListener("click", function(item) {
-        if (item.target && item.target.tagName === "LI") {
-            item.target.classList.toggle("checked");
-        }
-    });
 }
-
-function createDeleteButton(item) {
-    let span = document.createElement("SPAN");
-    let txtNode = document.createTextNode("\u00D7");
-    
-    span.className = "close";
-    span.appendChild(txtNode);
-    span.contentEditable = "false";
-    span.addEventListener('click', function() {
-        let test = this.parentElement;
-        test.remove();
-    });
-    item.appendChild(span);
-}
-
-function createEditButton(item) {
-    let span = document.createElement("SPAN");
-    let txtNode = document.createTextNode("edit");
-    
-    span.className = "edit";
-    span.appendChild(txtNode);
-    span.contentEditable = "false";
-    span.addEventListener('click', function() {
-        let test = this.parentElement;
-        switch(test.contentEditable) {
-            case "false":
-                test.contentEditable = "true";
-                break;
-            case "true":
-                test.contentEditable = "false";
-                break;
-            default:
-                test.contentEditable = "true";
-        }
-    });
-    item.appendChild(span);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
